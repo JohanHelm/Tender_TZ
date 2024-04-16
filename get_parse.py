b25960e4ef3c1d02a7799f4b71e7c2a077d49d6e
johan_helm <jou@ngs.ru>
2024-04-16 17:57:08 +0700

    task done with classes

--- get_parse.py
+++ get_parse.py
@@ -1,50 +1,37 @@
-from aiohttp.client import ClientSession
+from requests import Session
+from requests.models import Response
 from bs4 import BeautifulSoup
 from celery import Celery, Task
+from typing import Iterator
+
 
 app = Celery("get_parse", backend="redis://localhost:6379",  broker="redis://localhost:6379")
 
 
-headers = {
-    "User-Agent":
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"}
+class GetPageData(Task):
+    def run(self, link: str, session: Session) -> Response:
+        return session.get(link, timeout=3)
 
 
-async def get_main_page_data(link: str):
-    async with ClientSession() as session:
-        async with session.get(link, headers=headers, timeout=3) as response:
-            html_soup = BeautifulSoup(await response.text(), "lxml")
-            return html_soup
-
-
-async def parse_main_page(html_soup) -> list[str, ...]:
-    print_link_xml_list = []
+def parse_main_page(response: Response) -> Iterator[str]:
+    html_soup = BeautifulSoup(response.text, "lxml")
     print_blocks_list = html_soup.find_all("div",
                                            attrs={"class": "w-space-nowrap ml-auto registry-entry__header-top__icon"})
     for print_block in print_blocks_list:
         print_link_block = print_block.find("a", attrs={"target": "_blank"})
         part_print_link = print_link_block.get("href").replace("view.html", "viewXml.html")
         print_link_xml = f"https://zakupki.gov.ru{part_print_link}"
-        print_link_xml_list.append(print_link_xml)
-    return print_link_xml_list
+        yield print_link_xml
 
 
-@app.task
-async def get_parse_xml_form(link: str) -> str | None:
-    async with ClientSession() as session:
-        async with session.get(link, headers=headers, timeout=3) as response:
-            soup = BeautifulSoup(await response.text(), features="xml")
-            result = soup.find("publishDTInEIS")
-            if result:
-                return result.text
-            return result
+def parse_xml_form(response: Response) -> str | None:
+    soup = BeautifulSoup(response.text, features="xml")
+    result = soup.find("publishDTInEIS")
+    if result:
+        return result.text
+    return result
 
 
-@app.task
-def get_hello_world():
-    return "hello world"
-
-
-main_urls = ("https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=1",
-             "https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=2")
-# asyncio.run(get_html_data(main_urls[0]))
+class GetHelloWorld(Task):
+    def run(self):
+        return ["hello world"]
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from get_parse import get_main_page_data, get_parse_xml_form, parse_main_page, get_hello_world
+from requests import Session
+
+from get_parse import GetPageData, parse_xml_form, parse_main_page
 
 
-main_urls = ("https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=1",
-             "https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=2")
+headers = {
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"}
 
 
 def main():
-    result = get_hello_world.delay()
-    print(result.get())
+    main_urls = ("https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=1",
+                 "https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=2")
+    session = Session()
+    session.headers.update(headers)
+    gpd = GetPageData()
+    for url in main_urls:
+        html_response = gpd.run(url, session)
+        for xml_link in parse_main_page(html_response):
+            xml_response = gpd.run(xml_link, session)
+            print(xml_link, parse_xml_form(xml_response))
 
 
 if __name__ == "__main__":
     main()
-
